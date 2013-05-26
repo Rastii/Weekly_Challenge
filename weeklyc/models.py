884a34777b44be422084cf022c91268bce201f6c
Rastii <rastii09@gmail.com>
2013-05-25 20:01:58 -0400

    Weird

---
+++ weeklyc/models.py
@@ -0,0 +1,54 @@
+import datetime as dt
+from sqlalchemy import Table, Column, ForeignKey, Integer, String,\
+Boolean, Text, DateTime
+from sqlalchemy.orm import relationship, backref, deferred
+from weeklyc.database import Base, db_session
+from sqlalchemy.orm.collections import attribute_mapped_collection
+import datetime
+
+def now():
+    return datetime.datetime.now()
+
+user_submissions = Table('submissions', Base.metadata,
+    Column('challenge_id', Integer, ForeignKey('challenges.id')),
+    Column('user_id', Integer, ForeignKey('users.id')))
+
+class User(Base):
+    __tablename__ = 'users'
+    id          = Column(Integer, primary_key=True)
+    login       = Column(String(64), unique=True)
+    password    = deferred(Column(String(60)))
+    enabled     = deferred(Column(Boolean, default=True))
+    submissions = relationship("submissions", backref="users")
+
+    def __repr__(self):
+        return '< User(login: %s, enabled: %s) >' %\
+            (self.login, self.enabled)
+
+
+    def enable(self):
+        self.enabled = True
+
+    # These four are required helpers for flask user login
+    def get_id(self):
+        return unicode(self.id)
+
+    def is_active(self):
+        return self.enabled
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+class Challenge(Base):
+    __tablename__ = 'challenges'
+    id              = Column(Integer, primary_key=True)
+    name            = Column(String(128), unique=True)
+    flag            = Column(String(32))
+
+class Submission(Base):
+    __tablename__ = 'submissions'
+    user_id         = Column(Integer, ForeignKey('users.id'))
+    challenge       = Column(Integer, ForeignKey('challenges.id'))
